25b04405dbe7f192b4c65ecc4c0c0e6b6cd5ea42
AsBergs <anasofiabergsoares06@gmail.com>
2022-09-08 14:32:25 -0300

    gfdgd

--- bookshelf/src/api/categorias.js
+++ bookshelf/src/api/categorias.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 export default class CategoriasApi {
   async buscarTodosOsCategorias() {
-    const response = await axios.get('http://localhost:4000/categorias');
+    const response = await axios.get('https://bookshelf-gobetti.herokuapp.com/categorias');
     return response.data;
   }
 
   async buscarCategoria(id) {
-    const response = await axios.get(`http://localhost:4000/categorias/${id}`);
+    const response = await axios.get(`https://bookshelf-gobetti.herokuapp.com/categorias/${id}`);
     return response.data;
   }
 
   async adicionarCategoria(categoria) {
-    const response = await axios.post('http://localhost:4000/categorias', categoria);
+    const response = await axios.post('https://bookshelf-gobetti.herokuapp.com/categorias', categoria);
     return response.data;
   }
 
   async excluirCategoria(id) {
-    const response = await axios.delete(`http://localhost:4000/categorias/${id}`);
+    const response = await axios.delete(`https://bookshelf-gobetti.herokuapp.com/categorias/${id}`);
     return response.data;
   }
 
   async atualizarCategoria(categoria) {
     const response = await axios.put(
-      `http://localhost:4000/categorias/${categoria.id}`,
+      `https://bookshelf-gobetti.herokuapp.com/categorias/${categoria.id}`,
       categoria,
     );
     return response.data;
--- bookshelf/src/api/editoras.js
+++ bookshelf/src/api/editoras.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 export default class EditorasApi {
   async buscarTodosOsEditoras() {
-    const response = await axios.get('http://localhost:4000/editoras');
+    const response = await axios.get('https://bookshelf-gobetti.herokuapp.com/editoras');
     return response.data;
   }
 
   async buscarEditora(id) {
-    const response = await axios.get(`http://localhost:4000/editoras/${id}`);
+    const response = await axios.get(`https://bookshelf-gobetti.herokuapp.com/editoras/${id}`);
     return response.data;
   }
 
   async adicionarEditora(editora) {
-    const response = await axios.post('http://localhost:4000/editoras', editora);
+    const response = await axios.post('https://bookshelf-gobetti.herokuapp.com/editoras', editora);
     return response.data;
   }
 
   async excluirEditora(id) {
-    const response = await axios.delete(`http://localhost:4000/editoras/${id}`);
+    const response = await axios.delete(`https://bookshelf-gobetti.herokuapp.com/editoras/${id}`);
     return response.data;
   }
 
   async atualizarEditora(editora) {
     const response = await axios.put(
-      `http://localhost:4000/editoras/${editora.id}`,
+      `https://bookshelf-gobetti.herokuapp.com/editoras/${editora.id}`,
       editora,
     );
     return response.data;
